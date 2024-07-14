7962c49587915da79ed927a1721250f3fcbfd484
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-07-15 01:18:54 +0500

    show users in dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'User',
+            'email' => 'user@example.com',
+        ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]);
+        User::factory()->create([
+            'name' => 'Test',
             'email' => 'test@example.com',
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
+Route::get('/dashboard', function () {
+    $users = User::where('id', '!=', auth()->user()->id)->get();
+    return view('dashboard', [
+        'users' => $users
+    ]);
+})->middleware(['auth', 'verified'])->name('dashboard');
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
