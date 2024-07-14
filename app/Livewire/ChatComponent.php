b51a270afb4a6d595dfa8b084d196292504c536c
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-07-15 01:55:20 +0500

    save messages in db

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -2,18 +2,33 @@
 
 namespace App\Livewire;
 
+use App\Models\Message;
 use App\Models\User;
 use Livewire\Component;
 
 class ChatComponent extends Component
 {
-    public $user;
+    public $user, $sender_id, $receiver_id, $message;
     public function mount($user_id)
     {
+        $this->sender_id = auth()->user()->id;
+        $this->receiver_id = $user_id;
         $this->user = User::whereId($user_id)->first();
     }
     public function render()
     {
         return view('livewire.chat-component');
+    }
+
+    public function sendMessage()
+    {
+        $chatMessage = new Message();
+        $chatMessage->sender_id = $this->sender_id;
+        $chatMessage->receiver_id = $this->receiver_id;
+        $chatMessage->message = $this->message;
+        $chatMessage->save();
+
+        $this->message = '';
+
     }
 }
