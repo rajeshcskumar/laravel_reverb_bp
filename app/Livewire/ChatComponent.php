9d69f5dac3c821f203eab36843ff96dc47c1d1e4
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-07-15 02:14:49 +0500

    get message query

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -8,11 +8,22 @@ use Livewire\Component;
 
 class ChatComponent extends Component
 {
-    public $user, $sender_id, $receiver_id, $message;
+    public $user, $sender_id, $receiver_id, $message, $messages = [];
     public function mount($user_id)
     {
         $this->sender_id = auth()->user()->id;
         $this->receiver_id = $user_id;
+
+        $messages = Message::where(function ($query) {
+            $query->where('sender_id', $this->sender_id)
+                ->where('receiver_id', $this->receiver_id);
+        })->orWhere(function ($query) {
+            $query->where('receiver_id', $this->receiver_id)
+                ->where('sender_id', $this->sender_id);
+        })
+            ->with('sender:id,name', 'receiver:id,name')
+            ->get();
+
         $this->user = User::whereId($user_id)->first();
     }
     public function render()
